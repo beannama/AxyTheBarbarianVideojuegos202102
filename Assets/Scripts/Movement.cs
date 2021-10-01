de87f4990e463b9f6f21db98ad89a6362cb4ca04
Benjamín Naranjo <Ben.Nar.95@gmail.com>
2021-10-01 19:09:31 -0300

    Part 1 done, check the movement frames

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Vector3 pos;                        // For movement
+    public float speed = 2.0f;          // Speed of movement
+
+    void Start()
+    {
+        pos = transform.position;       // Take the initial position
+    }
+
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        if (xInput > 0.01f && transform.position == pos) {       
+            pos += Vector3.right;
+        }
+        else if (xInput < -0.01f && transform.position == pos) { 
+            pos += Vector3.left;
+        }
+        else if (yInput > 0.01f && transform.position == pos) {
+            pos += Vector3.up;
+        }
+        else if (yInput < -0.01f && transform.position == pos) {
+            pos += Vector3.down;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);    // Move there
+    }
+}
---
+++ Assets/Scripts/OptionalScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionalScript : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public GameObject player;
+    private bool backgroundBoolean;
+    private bool characterBoolean;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GameObject.Find("Main Camera");
+        player = GameObject.Find("Player");
+        backgroundBoolean = false;
+        characterBoolean = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("j") && !backgroundBoolean)
+        {
+            backgroundBoolean = true;
+            mainCamera.GetComponent<Camera>().backgroundColor = Color.green;
+        }
+        else if (Input.GetKeyDown("j") && backgroundBoolean)
+        {
+            backgroundBoolean = false;
+            mainCamera.GetComponent<Camera>().backgroundColor = Color.cyan;
+            
+        }
+
+        if (Input.GetKeyDown("k") && !characterBoolean)
+        {
+            characterBoolean = true;
+            player.GetComponent<SpriteRenderer>().color = Color.black;
+        }
+        else if (Input.GetKeyDown("k") && characterBoolean)
+        {
+            characterBoolean = false;
+            player.GetComponent<SpriteRenderer>().color = Color.white;
+
+        }
+    }
+}
